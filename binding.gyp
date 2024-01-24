dfc878cee2f3e48c642f3965dd2f8ea685ece5b9
FurWaz <fur.waz06@gmail.com>
2024-01-24 23:01:46 +0100

    Changed from cmake to node-gyp configuration

---
+++ binding.gyp
@@ -0,0 +1,31 @@
+{
+  "targets": [
+    {
+      "target_name": "fullbowody",
+      "cflags!": [ "-fno-exceptions" ],
+      "cflags_cc!": [ "-fno-exceptions" ],
+      "sources": [
+        "./src/main.cpp"
+      ],
+      "include_dirs": [
+        "<!@(node -p \"require('node-addon-api').include\")",
+        "<(module_root_dir)/include"
+      ],
+      'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ],
+      "conditions": [
+        ['OS == "win"',
+          {
+            "link_settings": {
+                "libraries": [
+                    
+                ],
+                "library_dirs": [
+                  "<(module_root_dir)/lib"
+                ]
+            }
+          }
+        ]
+      ]
+    }
+  ]
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,44 @@
-#include <iostream>
-#include <windows.h>
+#include <napi.h>
 #include "Engine/EngineLoader.hpp"
 
 const std::string ENGINE_PATH = "C:/Users/furwa/Documents/FurWaz/FullBowody/Engine/build/Debug/Engine.dll";
 
-int main(int argc, char const *argv[])
+EngineLoader loader(ENGINE_PATH);
+Engine* engine = nullptr;
+
+void registerFunction(Napi::Object exports, Napi::Env env, std::string name, Napi::Value (*function)(const Napi::CallbackInfo&))
 {
-    EngineLoader loader(ENGINE_PATH);
-    Engine* engine = loader.createEngine();
-
-    engine->Run();
-
-    loader.destroyEngine(engine);
+    exports.Set(Napi::String::New(env, name), Napi::Function::New(env, function));
 }
+
+Napi::Value Init(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    engine = loader.createEngine();
+    return Napi::Boolean::New(env, true);
+}
+
+Napi::Value Run(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    engine->Run();
+    return Napi::Boolean::New(env, true);
+}
+
+Napi::Value Shutdown(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    loader.destroyEngine(engine);
+    return Napi::Boolean::New(env, true);
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    registerFunction(exports, env, "init", Init);
+    registerFunction(exports, env, "run", Run);
+    registerFunction(exports, env, "shutdown", Shutdown);
+
+    return exports;
+}
+
+NODE_API_MODULE(FullBowody, Init);
