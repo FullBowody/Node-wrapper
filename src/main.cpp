2a15655baee0fab9fb56e28e2abd5c90663e1a92
FurWaz <fur.waz06@gmail.com>
2024-04-16 20:08:36 +0200

    Improved dynamic engine loading and updated submodule

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,46 @@
 #include "EngineWrap.hpp"
 #include "CameraWrap.hpp"
 
-#ifdef _WIN32
-const std::string ENGINE_PATH = "./Engine.dll";
-#else
-const std::string ENGINE_PATH = "./Engine.so";
-#endif
-
-EngineLoader loader = EngineLoader(ENGINE_PATH);
+EngineLoader* loader = nullptr;
 
 #define register(x, y) exports.Set(Napi::String::New(env, x), Napi::Function::New(env, y))
 
 Napi::Value CreateEngine(const Napi::CallbackInfo& info)
 {
     Napi::Env env = info.Env();
-    Engine* engine = loader.createEngine();
+
+    if (loader != nullptr)
+    {
+        std::cerr << "Error : Engine already created" << std::endl;
+        return env.Null();
+    }
+
+    if (info.Length() > 0)
+        loader = new EngineLoader(info[0].As<Napi::String>().Utf8Value());
+    else loader = new EngineLoader();
+
+    Engine* engine = loader->createEngine();
+    if (engine == nullptr)
+    {
+        std::cerr << "Error : Engine creation failed" << std::endl;
+        loader = nullptr;
+        return env.Null();
+    }
     return EngineWrap::NewInstance(env, engine);
 }
 
 Napi::Value DestroyEngine(const Napi::CallbackInfo& info)
 {
     Napi::Env env = info.Env();
+    if (loader == nullptr)
+    {
+        std::cerr << "Error : Engine not created" << std::endl;
+        return Napi::Boolean::New(env, false);
+    }
+
     Napi::Object wrapper = info[0].As<Napi::Object>();
     EngineWrap* engineWrap = Napi::ObjectWrap<EngineWrap>::Unwrap(wrapper);
-    loader.destroyEngine(engineWrap->getEngine());
+    loader->destroyEngine(engineWrap->getEngine());
     return Napi::Boolean::New(env, true);
 }
 
